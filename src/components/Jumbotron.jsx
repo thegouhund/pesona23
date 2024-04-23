cb7fe7eade98944c36e042632461c27e10e44623
thegouhund <alexandresapu1@gmail.com>
2024-04-24 02:05:42 +0800

    jumbotron

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -31,10 +31,10 @@ const Jumbotron = () => {
         <h1 className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] mb-4 mt-64 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
           PESONA 23
         </h1>
-        <p className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] mb-2 font-semibold text-lg text-gray-200 lg:text-2xl sm:px-16 lg:px-48">
+        <p className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] mb-2 font-semibold text-lg text-white lg:text-2xl sm:px-16 lg:px-48">
           Pengabdian Mahasiswa Untuk Edukasi, Sosialisasi dan Aksi Nyata.
         </p>
-        <p className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] text-md font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+        <p className="[text-shadow:_0_1px_0_rgb(0_0_0_/_40%)] text-md font-normal text-white lg:text-xl sm:px-16 lg:px-48">
           27-28 April 2024
         </p>
       </div>
