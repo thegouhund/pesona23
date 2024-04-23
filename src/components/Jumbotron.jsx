bd959922bc33abb6b3ff4bc1090179e8165d8b73
kurniadinata <kurniadinatarizky@gmail.com>
2024-04-23 23:20:07 +0800

    liat sendiri

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,31 +1,46 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
 const Jumbotron = () => {
-  return (
-    <section
-      id="jumbotron"
-      className="bg-cover bg-no-repeat bg-[url('src/assets/jumbo_img.jpeg')] h-screen"
-    >
-      <div className="mx-auto text-center py-24 drop-shadow-2xl">
-        <h1 className="mb-4 mt-64 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-          PESONA 23
-        </h1>
-        <p className="mb-2 font-semibold text-lg text-gray-200 lg:text-2xl sm:px-16 lg:px-48">
-          Pengabdian Mahasiswa Untuk Edukasi, Sosialisasi dan Aksi Nyata.
-        </p>
-        <p className="text-md font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-          27-28 April 2024
-        </p>
-      </div>
-      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#FFFFFF"
-          fill-opacity="1"
-          d="M0,128L48,160C96,192,192,256,288,245.3C384,235,480,149,576,128C672,107,768,149,864,165.3C960,181,1056,171,1152,149.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-        ></path>
-      </svg> */}
-    </section>
-  );
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const images = [
+		"https://images2.imgbox.com/1c/f8/uXnE7S5T_o.jpeg",
+		"https://images2.imgbox.com/fd/e4/NLeNFFUZ_o.jpeg",
+	];
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+		}, 2000);
+		return () => {
+			if (interval) {
+				clearInterval(interval);
+			}
+		};
+	}, [currentIndex]);
+
+	return (
+		<div
+			id="jumbotron"
+			style={{
+				backgroundImage: `url(${images[currentIndex]})`,
+				animationDuration: 2000,
+			}}
+			className="bg-cover bg-no-repeat h-screen drop-shadow-2xl"
+		>
+			<div className="mx-auto text-center py-24 drop-shadow-2xl">
+				<h1 className="mb-4 mt-64 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
+					PESONA 23
+				</h1>
+				<p className="mb-2 font-semibold text-lg text-gray-200 lg:text-2xl sm:px-16 lg:px-48">
+					Pengabdian Mahasiswa Untuk Edukasi, Sosialisasi dan Aksi
+					Nyata.
+				</p>
+				<p className="text-md font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
+					27-28 April 2024
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Jumbotron;
--- src/components/Tujuan.jsx
+++ src/components/Tujuan.jsx
@@ -1,42 +1,46 @@
 import handshakeImg from "../assets/handshake.png";
-import hatImg from "../assets/topi_wisuda.png";
-import solidImg from "../assets/solid.png";
+import hatImg from "../assets/education.png";
+import solidImg from "../assets/solidarity.png";
 import Title from "./Title";
 
 const Tujuan = () => {
-  return (
-    <>
-      <Title>Tujuan Acara</Title>
-      <div className="mx-4 flex max-md:flex-col md:gap-8 justify-center items-stretch max-md:items-center">
-        <div className="bg-[#7BCB6B] rounded-3xl flex flex-col justify-center items-center mb-8 p-4 text-center w-80 aspect-square">
-          <h4 className="text-xl font-bold">Pengenalan</h4>
-          <p className="mb-16">
-            Mengenal satu sama lain mahasiswa se2luruh program studi angkatan
-            2023 Institut Teknologi Kalimantan.
-          </p>
-          <img src={handshakeImg} className="w-1/2" />
-        </div>
+	return (
+		<>
+			<div class="flex justify-center  m-20">
+				<h1 class="mb-4 text-3xl font-extrabold tracking-tight leading-none text-default md:text-3xl lg:text-5xl text-center">
+					Tujuan Acara
+				</h1>
+			</div>
+			<div className="mx-4 flex max-md:flex-col md:gap-8 justify-center items-stretch max-md:items-center">
+				<div className="rounded-3xl flex flex-col justify-center items-center mb-8 p-4 text-center w-80 aspect-square">
+					<img src={handshakeImg} className="w-1/2 " />
+					<h4 className="text-xl font-bold">Pengenalan</h4>
+					<p className="mb-16">
+						Mengenal satu sama lain mahasiswa seluruh program studi
+						angkatan 2023 Institut Teknologi Kalimantan.
+					</p>
+				</div>
 
-        <div className="bg-[#FF7F8A] rounded-3xl flex flex-col justify-center items-center mb-8 p-4 text-center w-80 aspect-square">
-          <h4 className="text-xl font-bold">Peran Mahasiswa</h4>
-          <p className="mb-16">
-            Menjalankan peran sebagai mahasiswa yang peduli terhadap masyarakat
-            dan lingkungan sekitar.
-          </p>
-          <img src={hatImg} className="w-1/2" />
-        </div>
+				<div className="rounded-3xl flex flex-col justify-center items-center mb-8 p-4 text-center w-80 aspect-square">
+					<img src={hatImg} className="w-1/2" />
+					<h4 className="text-xl font-bold">Peran Mahasiswa</h4>
+					<p className="mb-16">
+						Menjalankan peran sebagai mahasiswa yang peduli terhadap
+						masyarakat dan lingkungan sekitar.
+					</p>
+				</div>
 
-        <div className="bg-[#FFAD61] rounded-3xl flex flex-col justify-center items-center mb-8 p-4 text-center w-80 aspect-square">
-          <h4 className="text-xl font-bold">Solidaritas</h4>
-          <p className="mb-16">
-            Memperkuat rasa solidaritas antar sesama mahasiswa angkatan 2023
-            Institut Teknologi Kalimantan.
-          </p>
-          <img src={solidImg} className="w-1/2" />
-        </div>
-      </div>
-    </>
-  );
+				<div className="rounded-3xl flex flex-col justify-center items-center mb-8 p-4 text-center w-80 aspect-square">
+					<img src={solidImg} className="w-1/2" />
+					<h4 className="text-xl font-bold">Solidaritas</h4>
+					<p className="mb-16">
+						Memperkuat rasa solidaritas antar sesama mahasiswa
+						angkatan 2023 Institut Teknologi Kalimantan.
+					</p>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Tujuan;
