7dabc5c1624fb3a88aa9b608af94bbeafadd02f6
thegouhund <alexandresapu1@gmail.com>
2024-04-24 21:22:12 +0800

    jumbotron to local image

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,11 +1,20 @@
 import { useState, useEffect } from "react";
+import jumbotron1 from "../assets/jumbotron1.jpeg";
+import jumbotron2 from "../assets/jumbotron2.jpeg";
 
 const Jumbotron = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const images = [
-    "https://images2.imgbox.com/1c/f8/uXnE7S5T_o.jpeg",
-    "https://images2.imgbox.com/fd/e4/NLeNFFUZ_o.jpeg",
-  ];
+  // const images = [
+  //   "https://images2.imgbox.com/1c/f8/uXnE7S5T_o.jpeg",
+  //   "https://images2.imgbox.com/fd/e4/NLeNFFUZ_o.jpeg",
+  // ];
+
+  // const images = [
+  //   "https://images2.imgbox.com/a0/74/L6YvdIYs_o.jpeg",
+  //   "https://images2.imgbox.com/82/76/8BoBtm0u_o.jpeg",
+  // ];
+
+  const images = [jumbotron1, jumbotron2];
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -37,7 +46,6 @@ const Jumbotron = () => {
           <img
             key={index}
             src={image}
-            alt={`Slide ${index + 1}`}
             className={`absolute top-0 left-0 w-full h-full transition-opacity duration-1000 brightness-50 object-cover ${
               index === currentIndex ? "opacity-100" : "opacity-0"
             }`}
