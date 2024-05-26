cc3ae02ac2326469b6a52a2a4de4ea58f33a63fe
thegouhund <alexandresapu1@gmail.com>
2024-05-27 07:47:16 +0800

    usePublic

--- src/pages/PdfViewer.jsx
+++ src/pages/PdfViewer.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 const PdfViewer = ({file, title}) => {
-  file = "https://filebin.net/i7dpljfryt1z686v/sop.pdf"
   return (
     <>
       <Navbar isSop={true} />
