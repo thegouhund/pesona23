49e5bf17ae1b20d063f254f634f414db69655870
thegouhund <alexandresapu1@gmail.com>
2024-05-27 13:17:58 +0800

    resize pdf viewer title

--- src/pages/PdfViewer.jsx
+++ src/pages/PdfViewer.jsx
@@ -6,12 +6,12 @@ const PdfViewer = ({ file, title }) => {
   return (
     <>
       <Navbar isSop={true} />
-      <main className="flex flex-col justify-center items-center mt-28">
-        <h1 className="text-3xl font-extrabold tracking-tight leading-none md:text-3xl lg:text-5xl text-center">
+      <main className="flex flex-col justify-center items-center mt-32">
+        <h1 className="text-xl font-extrabold tracking-tight leading-none md:text-2xl lg:text-5xl text-center mb-8">
           {title}
         </h1>
         <iframe
-          className="w-screen mx-auto px-8 md:px-16 md:h-[1500px] h-[750px]"
+          className="w-screen px-8 md:px-16 md:h-[1500px] h-[750px]"
           src={file}
         ></iframe>
         <a
